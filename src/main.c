78f8ce3ca4646fe4577ebe8e51b88d46c7faf481
mondbaeraut <nfe3303@students.fhv.at>
2017-03-21 19:00:07 +0100

    Blink

--- src/main.c
+++ src/main.c
@@ -1,17 +1,20 @@
 /*
  * Blink LED
  */
-
+#include <inttypes.h>
 //This register controls the PER domain power state transition.
-#define PM_PWSTCTRL_PER (long*)0x483070E0
+#define PM_PWSTCTRL_PER (volatile uint32_t*)0x483070E0
 //Controls the modules functional clock activity.
-#define CM_FCLKEN_PER (long*)0x48005000
+#define CM_ICLKEN_PER (volatile uint32_t*)0x48005010
 
-#define GPIO5_DATAOUT (long*)0x4905603C
-#define GPIO5_OE (long*)0x49056034
-#define GPIO5_CTRL (long*)0x49056030
+#define GPIO5_SETDATAOUT (volatile uint32_t*) 0x49056094
+#define GPIO5_CLEARDATAOUT (volatile uint32_t*) 0x49056090
+#define GPIO5_OE (volatile uint32_t*)0x49056034
 
-#define LED1 (1<<21)
+// register to set mux mode (bit 16-18)
+#define LED_GREEN_MUXMODE (volatile uint32_t*) 0x4800217C
+
+#define LED_GREEN (1<<21)
 
 int main(void)
 {
@@ -19,22 +22,30 @@ int main(void)
     /*
      * System
      */
-    *PM_PWSTCTRL_PER |= (1 << 0); //Set power state on
-    *PM_PWSTCTRL_PER |= (1 << 1); //0x03 Power On
-    *CM_FCLKEN_PER |= (1 << 16);    //enable functional clock for gpio5
+    *PM_PWSTCTRL_PER |= ((1 << 0) | (1 << 1)); //Set power state on 0x03 Power On
+    *CM_ICLKEN_PER |= (1 << 16);
+
+    // set mux mode to 4 (GPIO)
+    *LED_GREEN_MUXMODE &= ~(1 << 16);
+    *LED_GREEN_MUXMODE &= ~(1 << 17);
+    *LED_GREEN_MUXMODE |= (1 << 18);
     /*
      * GPIO
      */
-    *GPIO5_OE |= ~LED1; //set GPIO5_OE pin 16 on 0 -> OutputPin
-    *GPIO5_DATAOUT &= ~LED1;    //turn off led
+
+    *GPIO5_OE &= ~LED_GREEN; //set GPIO5_OE pin 16 on 0 -> OutputPin
 
     while (1)
     {
-        for (i = 0; i < 2000000L; i++)
+        for (i = 0; i < 100000L; i++)
         {
-
         }
-        *GPIO5_DATAOUT ^= ~LED1;
+        // led on
+        *GPIO5_SETDATAOUT |= LED_GREEN;
+        for (i = 0; i < 100000L; i++)
+        {
+        }
+        *GPIO5_CLEARDATAOUT |= LED_GREEN;
     }
     return 0;
 }
